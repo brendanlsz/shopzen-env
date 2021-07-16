7dc1b87d3ade08cdf79a99e6315fb743d6b276c8
naijie2108 <yeatnaijie@gmail.com>
2021-07-16 18:39:09 +0800

    username

--- src/components/AdminInformation/index.js
+++ src/components/AdminInformation/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./styles.scss";
 
 const AdminInformation = (props) => {
-  const { displayName, email } = props;
+  const { userName, email } = props;
   return (
     <div className="admin-information">
       <div className="profileicon">
         <i class="fa fa-user" aria-hidden="true"></i>
       </div>
       <div className="details">
-        <p>Username: {displayName}</p>
+        <p>Username: {userName}</p>
         <p>E-mail: {email}</p>
       </div>
     </div>
