29f072a551a3683df24d12357fc1c638a21c8d15
brendanlsz <brendanlau2926@gmail.com>
2021-07-22 09:26:18 +0800

    added greetings based on time

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useHistory } from "react-router-dom";
 import "./styles.scss";
 import userIMG from "./../../assets/user.png";
@@ -6,6 +6,19 @@ import userIMG from "./../../assets/user.png";
 const UserProfile = (props) => {
   const { currentUser } = props;
   const { userName } = currentUser;
+  const [time, setTime] = useState(0)
+  var today = new Date()
+  var curHr = today.getHours()
+
+  useEffect(() => {
+    if (curHr < 12) {
+      setTime(0);
+    } else if (curHr < 18) {
+      setTime(1);
+    } else {
+      setTime(2);
+    }
+  }, [curHr])
 
   return (
     <div className="userProfile">
@@ -16,7 +29,16 @@ const UserProfile = (props) => {
           </div>
         </li>
         <li>
-          <span className="displayName">{userName && userName}</span>
+          {time == 0 && (
+            <span className="displayName1">Good Morning,</span>
+          )}
+          {time == 1 && (
+            <span className="displayName1">Good Afternoon,</span>
+          )}
+          {time == 2 && (
+            <span className="displayName1">Good Evening,</span>
+          )}
+          <span className="displayName"> {userName && userName}</span>
         </li>
       </ul>
     </div>
